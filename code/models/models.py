2727058f1873d6d969eead5defda2b7e2c3227f8
robertofranceschi <roberto.franceschi@studenti.polito.it>
2020-06-09 17:44:27 +0200

    upload from local repo

--- code/models/models.py
+++ code/models/models.py
@@ -1,7 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+from torch.autograd import Function
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
 
 # AlexNet modified version with DANN implementation
 class ReverseLayerF(Function):
@@ -61,6 +68,8 @@ class DANN(nn.Module):
     def forward(self, x, alpha=None):
         features = self.features(x)
         # Flatten the features:
+        #features = self.avgpool(features)
+        #features = torch.flatten(features,1)
         features = features.view(features.size(0), -1)
         # If we pass alpha, we can assume we are training the discriminator
         if alpha is not None:
@@ -72,3 +81,24 @@ class DANN(nn.Module):
         else:
             class_outputs = self.classifier(features)
             return class_outputs
+
+def dann_net(pretrained=False, progress=True, **kwargs):
+    """AlexNet model architecture 
+    Args:
+        pretrained (bool):  If True, returns a model pre-trained on ImageNet
+        progress (bool):    If True, displays a progress bar of the download to stderr
+    """
+    net = DANN(**kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls['alexnet'], progress=progress)
+        net.load_state_dict(state_dict, strict = False)
+
+
+        # Copy pretrained weights from the classifier to the domain_classifier
+        net.dann_classifier[1].weight.data = net.classifier[1].weight.data.clone()
+        net.dann_classifier[1].bias.data = net.classifier[1].bias.data.clone()
+
+        net.dann_classifier[4].weight.data = net.classifier[4].weight.data.clone()
+        net.dann_classifier[4].bias.data = net.classifier[4].bias.data.clone()
+        
+    return net
--- code/utils/utils.py
+++ code/utils/utils.py
@@ -4,6 +4,39 @@ import torch
 import os
 
 # SHOW SOME RANDOM IMAGES
+def plotBar(data1, data2, data3, data4, classes, color='#3949AB'):
+	unique, counts = np.unique(data, return_counts=True)
+	print(unique)
+	print(counts)
+	print('Mean images per class:', counts.mean())
+	print('Std images per class:', counts.std())
+
+	unique, counts1 = np.unique(data1, return_counts=True)
+	unique, counts2 = np.unique(data2, return_counts=True)
+	unique, counts3 = np.unique(data3, return_counts=True)
+	unique, counts4 = np.unique(data4, return_counts=True)
+
+	fig, ax = plt.subplots(nrows=1, ncols=1)
+	ax.set_title('Distribution of classes across domains', pad=20.0, alpha=0.85, fontweight='bold')
+
+	width=0.18
+
+	plt.bar(unique, counts, width=width, color=color)
+	plt.bar(unique-2*(width)+(width/2), counts1, width=width, color='#3949AB70', edgecolor='#3949AB95', linewidth=0.5, label='Photo')
+	plt.bar(unique-(width/2), counts2, width=width, color='#f4433670', edgecolor='#f4433695', linewidth=0.5, label='Art paintings')
+	plt.bar(unique+(width/2), counts3, width=width, color='#388E3C70', edgecolor='#388E3C95', linewidth=0.5, label='Cartoon')
+	plt.bar(unique+2*(width)-(width/2), counts4, width=width, color='#FF8F0070', edgecolor='#FF8F0095', linewidth=0.5, label='Sketch')
+
+	plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
+
+	ax.set_xticks(unique)
+	ax.set_xticklabels([(classe[0].upper()+classe[1:]) for classe in classes])
+
+	plt.grid(alpha=0.2, axis='y')
+
+	ax.legend()
+	plt.show()
+
 def show_random_images(dataset, n=5, mean=None, std=None):
 	for i in range(n):
 	    j = np.random.randint(0, len(dataset))
@@ -14,17 +47,22 @@ def show_random_images(dataset, n=5, mean=None, std=None):
 
 def imgshow(img, mean=None, std=None):
 	if mean == None or std == None:
-		raise RuntimeError("You should pass mean and std to 'imgshow' method")
+		# use (0.5 0.5 0.5) (0.5 0.5 0.5) as mean and std
+		img = img / 2 + 0.5     # unnormalize
+		npimg = img.numpy()
+		plt.imshow(np.transpose(npimg, (1, 2, 0)))
+		plt.show()
+		# raise RuntimeError("You should pass mean and std to 'imgshow' method")
+	else : 
+		# use custom mean and std computed on the images
+		mean = np.array(mean)
+		std = np.array(std)
+		for i in range(3): 
+			img[i] = img[i]*std[i] + mean[i] # unnormalize
 
-	mean = np.array(mean)
-	std = np.array(std)
-	for i in range(3): 
-		img[i] = img[i]*std[i] + mean[i] # unnormalize
-
-	npimg = img.numpy()
-	plt.imshow(np.transpose(npimg, (1, 2, 0)))
-	plt.show()
-
+		npimg = img.numpy()
+		plt.imshow(np.transpose(npimg, (1, 2, 0)))
+		plt.show()
 	return
 
 def beep():

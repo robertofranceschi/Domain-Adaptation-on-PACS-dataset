c4e951ede3d3acaaa284555f8e873ae4e408a2d7
robertofranceschi <roberto.franceschi@studenti.polito.it>
2020-06-10 15:26:21 +0200

    upload from local repo

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -4,39 +4,6 @@ import torch
 import os
 
 # SHOW SOME RANDOM IMAGES
-def plotBar(data1, data2, data3, data4, classes, color='#3949AB'):
-	unique, counts = np.unique(data, return_counts=True)
-	print(unique)
-	print(counts)
-	print('Mean images per class:', counts.mean())
-	print('Std images per class:', counts.std())
-
-	unique, counts1 = np.unique(data1, return_counts=True)
-	unique, counts2 = np.unique(data2, return_counts=True)
-	unique, counts3 = np.unique(data3, return_counts=True)
-	unique, counts4 = np.unique(data4, return_counts=True)
-
-	fig, ax = plt.subplots(nrows=1, ncols=1)
-	ax.set_title('Distribution of classes across domains', pad=20.0, alpha=0.85, fontweight='bold')
-
-	width=0.18
-
-	plt.bar(unique, counts, width=width, color=color)
-	plt.bar(unique-2*(width)+(width/2), counts1, width=width, color='#3949AB70', edgecolor='#3949AB95', linewidth=0.5, label='Photo')
-	plt.bar(unique-(width/2), counts2, width=width, color='#f4433670', edgecolor='#f4433695', linewidth=0.5, label='Art paintings')
-	plt.bar(unique+(width/2), counts3, width=width, color='#388E3C70', edgecolor='#388E3C95', linewidth=0.5, label='Cartoon')
-	plt.bar(unique+2*(width)-(width/2), counts4, width=width, color='#FF8F0070', edgecolor='#FF8F0095', linewidth=0.5, label='Sketch')
-
-	plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
-
-	ax.set_xticks(unique)
-	ax.set_xticklabels([(classe[0].upper()+classe[1:]) for classe in classes])
-
-	plt.grid(alpha=0.2, axis='y')
-
-	ax.legend()
-	plt.show()
-
 def show_random_images(dataset, n=5, mean=None, std=None):
 	for i in range(n):
 	    j = np.random.randint(0, len(dataset))
@@ -64,6 +31,50 @@ def imgshow(img, mean=None, std=None):
 		plt.imshow(np.transpose(npimg, (1, 2, 0)))
 		plt.show()
 	return
+
+def plotImageDistribution(data1, data2, data3, data4, dataset_names, show=False):
+  # concatenate datasets
+  data = np.concatenate( (data1, data2, data3, data4) )
+  # count element per class
+  unique, counts = np.unique(data, return_counts=True)
+  # for each domain
+  unique, counts1 = np.unique(data1, return_counts=True)
+  unique, counts2 = np.unique(data2, return_counts=True)
+  unique, counts3 = np.unique(data3, return_counts=True)
+  unique, counts4 = np.unique(data4, return_counts=True)
+
+  if show: 
+    print('Total images:', np.sum(counts))
+    print('Number of classes:', len(unique))
+    print('Classes:', unique)
+    print('Total images per class:', counts)
+    print('Mean images per class:', counts.mean())
+    print('Std images per class:', counts.std())
+
+  print()
+  print('Element per class for each domain:')
+  for name,count in zip(dataset_names,[counts1,counts2,counts3,counts4]) : 
+    print(f'{name}_dataset: {count}')
+
+  fig, ax = plt.subplots(figsize=(10,7))
+
+  width=0.18
+
+  #plt.bar(unique, counts, width=width, color=color)
+  plt.bar(unique-2*(width)+(width/2), counts1, width=width, color='#FF8F77', linewidth=0.5, label='Photo')
+  plt.bar(unique-(width/2), counts2, width=width, color='#FFDF77', linewidth=0.5, label='Art paintings')
+  plt.bar(unique+(width/2), counts3, width=width, color='#8DF475', linewidth=0.5, label='Cartoon')
+  plt.bar(unique+2*(width)-(width/2), counts4, width=width, color='#77DCFF', linewidth=0.5, label='Sketch')
+
+  ax.set_xticks(unique)
+  classes = ['Dog', 'Elephant', 'Giraffe', 'Guitar', 'Horse', 'House', 'Person']
+  ax.set_xticklabels(classes)
+
+  plt.grid(alpha=0.2, axis='y')
+
+  plt.legend()
+  plt.show()
+  plt.savefig('distribution.png', dpi = 250)
 
 def beep():
 	# Play an audio beep. Any audio URL will do.
